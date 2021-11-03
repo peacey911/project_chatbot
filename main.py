cab7f37db0d228f7ba078e5780390c92c62db504
Peace (leed52) <leed52@coventry.ac.uk>
2021-11-03 00:11:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import time
+def sunny_bot():
+    print("Hello, my name is Sunny. Lovely to meet you.:]")
+    print("What should I call you?")
+sunny_bot()
+
+def error_m():
+    print("I couldn't understand that. Sorry :[")
+
+user = raw_input("My name is... >> ")
+print("Hi " + user + "I am your personal assistant chat bot")
+
+def start():
+    print("")
 
