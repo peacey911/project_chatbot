af58d387d5cdf7c5d0f6eb488c74bd4b65682b62
Peace (leed52) <leed52@coventry.ac.uk>
2021-11-04 05:54:35 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
+import time
 import datetime
-today =datetime.datetime.now()
+
+today = datetime.datetime.now()
 print(today)
 
-if today.strftime("%H") >= 00:
-    print("good night")
+# if today.strftime("%H") >= 00:
+#    print("good night")
+
+
+def start():
+    if 00 <= today.strftime("%H"):
+        print("Good morning!")
+    elif 12 <= today.strftime("%H"):
+        if today.strftime("%H") <= 7:
+            print("Good afternoon!")
+    else:
+        print("Good evening!")
+
+
+start()
