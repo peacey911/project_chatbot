a0e47d55a7091a3abb802861bedb4c85c8259222
Peace (leed52) <leed52@coventry.ac.uk>
2021-11-04 05:00:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,59 @@
+import time
 import datetime
+
 today = datetime.datetime.now()
 print(today)
+
+
+def error_m():
+    print("I couldn't understand that. Sorry :[")
+
+
+def end():
+    print("Good bye")
+
 
 def sunny_bot():
     print("Hello, my name is Sunny. Lovely to meet you.:]")
     time.sleep(1.5)
     print("What should I call you?")
+
+
 sunny_bot()
 
-def error_m():
-    print("I couldn't understand that. Sorry :[")
 
 user = raw_input("My name is... >> ")
-time.sleep(1.2)
+time.sleep(1.0)
 print("Hi " + user + " I am your personal assistant chat bot!")
 
-def start():
-    print("")
+
+def greeting():
+    if today.strftime("%H") >= 00:
+        print("Good morning!" + user + ":]")
+    elif 12 <= today.strftime("%H"):
+        if today.strftime("%H") <= 7:
+            print("Good afternoon! " + user + ":]")
+    else:
+        print("Good evening!" + user + ":]")
+
+
+a = raw_input(">>")
+if a == "what can you do":
+    print("Here is the options you can ask me. Try anything! ;]\n[greeting]")
+else:
+    error_m()
+
+# user's input
+b = raw_input(">>")
+if b == "greeting":
+    greeting()
+else:
+    error_m()
+    print("Here is the options you can ask me. Try anything! ;]\n[greeting]")
+
+
+
+
+
+
 
